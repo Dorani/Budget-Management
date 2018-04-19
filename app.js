a62ef3d40c228f598ca956d80ad0fefcca09ee56
Seif <seif.dorani@gmail.com>
2018-04-19 16:41:53 -0700

    power of closures

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-var budgetController = (function(){
-
-  var x = 23;
-  var add = function(a){
-    return x + a;
-  }
-  return {
-    publicTest: function(b) {
-      console.log(add(b));
-    }
-  }
-})();
-
 //When a javascript runtime hits the 1st line
 //it gets executed and the annomous function gets declared and immediately
 //called or invoked because of the operator on line 12
@@ -19,3 +6,33 @@ var budgetController = (function(){
   //this object gets assigned to the budgetController variable after the function returns
   //so... the budgetController variable is simply an object containing the method called:
   //publicTest
+
+
+var budgetController = (function(){
+
+  var x = 23;
+  var add = function(a){
+  return x + a;
+  }
+  return {
+    publicTest: function(b) {
+      console.log(add(b));
+    }
+  }
+})();
+
+
+var UIController = (function(){
+      //some code
+  });
+  //bridging the UI and the budgetController together
+var controller = (function(budegetCtrl, UICtrl){
+    var delta = budegetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function(){
+        console.log(delta);
+    }
+  }
+
+})(budgetController, UIController);
