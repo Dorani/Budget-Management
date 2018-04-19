0a6508a781f8c6e01e29e918ea196388ed65bc5f
Seif <seif.dorani@gmail.com>
2018-04-19 16:16:35 -0700

    explaing the flow of this function once executed

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ var budgetController = (function(){
     }
   }
 })();
+
+//When a javascript runtime hits the 1st line
+//it gets executed and the annomous function gets declared and immediately
+//called or invoked because of the operator on line 12
+  //then these variablesb and functions are declared starting line 3
+  //then we return an object which contains the method we call publicTest
+  //this object gets assigned to the budgetController variable after the function returns
+  //so... the budgetController variable is simply an object containing the method called:
+  //publicTest
