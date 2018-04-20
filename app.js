258aa845dd552aabd14fad6812f313f96dc839bd
Seif <seif.dorani@gmail.com>
2018-04-20 10:32:54 -0700

    3 input values stored in itemns, we should put them in an object though

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ var budgetController = (function(){
 
 //UI Controller
 var UIController = (function(){
-      //some code
+      return {
+        getinput: function(){
+            //3 input values stored in these variables
+            var type = document.querySelector(.'add__type').value;//read the value,income or expens
+            var description = document.querySelector('.add__description').value;
+            var value = doument.querySelector('add__value').value;
+        }
+      }
   });
 
 //Global App Controller
