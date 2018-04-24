e6cb17cae60da66e36a011ca8fd8b4927664d18e
Seif <seif.dorani@gmail.com>
2018-04-23 18:07:23 -0700

    UI stuff

--- app.js
+++ app.js
@@ -83,6 +83,16 @@ var UIController = (function(){
           };
         },
 
+        addListItem: function(obj, type){
+            //create html string with placeholder text
+            var html;
+            html = '<div class="item clearfix" id="income-0"> <div class="item__description">Salary</div> <div class="right clearfix"> <div class="item__value">+ 2,100.00</div> <div class="item__delete"><button class="item__delete--btn">i class="ion-ios-close-outline"></i></button> </div></div> </div>'
+
+            //replace the placeholder text with some actual Data
+
+            //inser the html in the dom
+        },
+
         getDomstrings: function(){
           //exposing Domstrings into the public
             return Domstrings;
