fc05baf7e8db9a82a8287b8c95c7aeb9a6330839
Seif <seif.dorani@gmail.com>
2018-04-23 23:53:56 -0700

    bug fixes

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ var UIController = (function(){
 
             if (type === 'inc') {
               element = Domstrings.incomeContainer;
-              html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"><button class="item__delete--btn">i class="ion-ios-close-outline"></i></button> </div></div> </div>'
+              html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div> </div>'
             } else if (type === 'exp') {
               element = Domstrings.expensesContainer;
               html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
